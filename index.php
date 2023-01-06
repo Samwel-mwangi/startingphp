baefab4f074387891420d67c6dae4db7a190492c
samwelmwangi <samuelmacharia879@gmail.com>
2023-01-06 21:33:06 +0300

    creating an environment

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echob"<h1>welcome to php</h1>";
+    echo "Hello world!";
+    echo "<br>";
+    $num1 = 5;
+    $num2 = 6;
+    $result= $num1 * $num2;
+    echo $result;
+     ?>
+</body>
+</html>
