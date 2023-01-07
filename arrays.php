84a96c341a6d99599d94f4682a2b1ca77a5eadee
samwelmwangi <samuelmacharia879@gmail.com>
2023-01-07 18:00:35 +0300

    arrays lesson

---
+++ arrays.php
@@ -0,0 +1,19 @@
+<?php
+$numbers=[1,2,3,4,5,6];
+var_dump($numbers);
+echo "<br>";
+$colors=["red","green","yellow"];
+print_r($colors);
+echo "<br";
+echo "9999999999 <br>";
+echo "<br>";
+echo $numbers[0];
+echo "<br>";
+$color=[
+    1 => "blue",
+    2 => "indigo",
+    3 => "magenta";
+];
+echo $color[1];
+//associative arrays
+?>
