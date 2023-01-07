8c9822ab641dbe67de9680c9ae107b0f28fd2e3f
samwelmwangi <samuelmacharia879@gmail.com>
2023-01-07 17:32:51 +0300

    variables

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+ 
     <?php 
-    echob"<h1>welcome to php</h1>";
+    echo"<h1>welcome to php</h1>";
     echo "Hello world!";
     echo "<br>";
     $num1 = 5;
     $num2 = 6;
     $result= $num1 * $num2;
     echo $result;
+    echo "<br>";
+    var_dump("Welcome to php");
+    echo "<br>";
+    var_dump([1,2,3,4,5]);
      ?>
-</body>
-</html>
