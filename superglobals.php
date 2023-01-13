d00c55e5165f7a4f2b4c3f3d2b4ec6986d6543c3
samwelmwangi <samuelmacharia879@gmail.com>
2023-01-13 21:21:58 +0300

    A lesson on post in php

---
+++ superglobals.php
@@ -0,0 +1,16 @@
+<?php 
+echo $_SERVER ['DOCUMENT_ROOT'];
+echo '<br>';
+echo $_SERVER ['SERVER_NAME'];
+echo $_SERVER ['SERVER_PORT'];
+echo $_SERVER ['REQUEST_URI'];
+echo $_SERVER ['SERVER_SOFTWARE'];
+echo '<br>';
+echo $_SERVER ['HTTP_USER_AGENT'];
+echo '<br';
+echo $_SERVER ['REMOTE_ADDR'];
+echo '<br>';
+echo $_SERVER ['REMOTE_PORT'];
+echo '<br>';
+
+?>
