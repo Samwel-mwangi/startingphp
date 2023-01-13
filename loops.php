b9918b7dd4bee530a81f570e0caf1515db9e021a
samwelmwangi <samuelmacharia879@gmail.com>
2023-01-13 19:07:52 +0300

    Loops assignment

--- loops.php
+++ loops.php
@@ -1,9 +1,14 @@
 <?php
-for($row=1;$row<=5;$row++){
-for($col=1;$col<=$row;$col++){
-    echo $col." ";
-    
-}
-echo "<br>";
-}
-?>
+$m = 5; 
+  // echo "m = " . $m . "<br>";
+  $count = 1;
+  for ($row = $m; $row > 0; $row--) 
+  {
+    for ($col = $row; $col < $m ; $col++) 
+     {
+       printf("%4s", $count);
+       $count++;
+     } 
+    echo "<br>";
+     }
+     ?>
